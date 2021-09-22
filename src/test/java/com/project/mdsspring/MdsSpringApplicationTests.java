81e723e7d0e35a81a72770848268d3d875e04679
Devalve <sekiro.shad@gmail.com>
2021-09-22 21:47:40 +0400

    init

---
+++ src/test/java/com/project/mdsspring/MdsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mdsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
