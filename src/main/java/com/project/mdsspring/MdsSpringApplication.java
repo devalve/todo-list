3e0f32b251ba833ae8f8b0435a067e948e7a7466
Devalve <sekiro.shad@gmail.com>
2021-10-04 09:26:21 +0400

    fix migration

--- src/main/java/com/project/mdsspring/MdsSpringApplication.java
+++ src/main/java/com/project/mdsspring/MdsSpringApplication.java
@@ -2,7 +2,6 @@ package com.project.mdsspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class MdsSpringApplication {
@@ -10,3 +9,4 @@ public class MdsSpringApplication {
         SpringApplication.run(MdsSpringApplication.class);
     }
 }
+
--- src/main/resources/db/migration/V20211001191040__init.sql
+++ src/main/resources/db/migration/V20211001191040__init.sql
@@ -2,13 +2,13 @@ create table todo.user
 (
     id          integer primary key,
     nickname    varchar(20) not null,
-    last_online timestamptz default now()
+    last_online_at timestamptz default now()
 );
-create table todo.todo
+create table todo.task
 (
     id         integer primary key,
     title      varchar not null,
-    text       text,
+    text       varchar,
     created_at timestamptz default now(),
-    author integer references "todo".user(id)
+    author_id integer references "todo".user(id)
 )
